5e54471b184f0d9006147ca1b6eba0122cfbaf38
Tausif Ahemed <professional.tausif011@gmail.com>
2024-02-21 13:34:30 +0530

    register problem

--- obj/Debug/net7.0/Bank.Management.Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Bank.Management.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Management.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feda0283a8cae37d149c7af12a93d5e43bc71ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Management.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Management.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
